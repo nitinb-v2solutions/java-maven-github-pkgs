22dfa043dce323298d90acbe22529889f3c1e306
Nitin Bhatnagar <nitin.bhatnagar@v2solutions.com>
2022-07-26 19:10:26 +0530

    Initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	V2Logger logger = new V2Logger();
+    	logger.logMessage("Hello World!");
+    }
+}
---
+++ src/main/java/com/example/V2Logger.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class V2Logger {
+
+	public void logMessage(String msg) {
+		
+		System.out.println("V2Logger:logMessage->"+msg);
+		
+	}
+}
