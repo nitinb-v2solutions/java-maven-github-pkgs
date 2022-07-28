a715db82bca5bb83024852c7cae90da12d91a5f2
Nitin Bhatnagar <nitin.bhatnagar@v2solutions.com>
2022-07-28 16:07:14 +0530

    testing gihub actions on commit and push to master branch

--- src/main/java/com/example/V2LoggerNew.java
+++ src/main/java/com/example/V2LoggerNew.java
@@ -4,7 +4,14 @@ public class V2LoggerNew {
 
 	public void logMessageNew(String msg) {
 		
-		System.out.println("V2Logger:logMessageNew->"+msg);
+		System.out.println("V2LoggerNew:logMessageNew->"+msg);
 		
 	}
+
+	public void logError(String msg) {
+		
+		System.out.println("V2LoggerNew:logError->"+msg);
+		
+	}
+
 }
